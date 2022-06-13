7b66b2b14d77479d7591b816a61d5af18b839d58
sahoo727 <soubhagyasahoo727@gmail.com>
2022-06-13 17:45:59 +0530

    started routes

--- server/index.js
+++ server/index.js
@@ -2,3 +2,21 @@ import express  from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use('/posts' , postRoutes);
+
+app.use(bodyParser.json({limit:"30mb" , extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb" , extended:true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://sahoo:sahoo@cluster0.sdswgyr.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL , {useNewUrlParser : true, useUnifiedTopology : true})
+    .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify',false);
